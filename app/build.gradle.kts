3e3d23e3b7a14d3fcc816b5b8db645b851fd5d01
David David <intellectualtechnology1@gmail.com>
2025-05-11 05:17:55 +0200

    add the slash page and refine the page still working on connecting to database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -29,22 +28,28 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/NOTICE.md"
+            excludes += "META-INF/LICENSE.md" // Added to resolve the duplicate file error
         }
     }
 }
@@ -58,17 +63,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.runtime)  // Corrected from runtime-android
+    implementation(libs.androidx.navigation.runtime)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui.android)
-    implementation("javax.mail:mail:1.4.7")
     implementation("androidx.compose.material:material-icons-extended:1.5.0")
-    implementation ("androidx.compose.material3:material3:1.3.0")
-    implementation ("androidx.compose.animation:animation:1.6.0")
+    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation("androidx.compose.animation:animation:1.6.0")
     implementation(libs.androidx.datastore.core.android)
     implementation(libs.generativeai)
     implementation(libs.androidx.material3.android)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -76,8 +79,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation("androidx.sqlite:sqlite:2.4.0") // If using SQLite directly
+    implementation("androidx.sqlite:sqlite:2.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.sun.mail:android-mail:1.6.7")
